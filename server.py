9333f9a0256cfab4961c35341701fa0f9e9b4b65
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-13 11:47:18 +0300

    Flask Templates

--- server.py
+++ server.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 print(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Farid is becoming a great programmer!'
+    return render_template('index.html')
+
+@app.route('/about.html')
+def aboutme():
+    return render_template('about.html')
+
+
+@app.route('/blog')
+def blog():
+    return 'this are my thought on blogs!'
+
+@app.route('/blog/2023/myFam')
+def blog2():
+    return 'I really love my Familly!'
