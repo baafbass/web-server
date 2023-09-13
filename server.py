ff66a7ba341fac941762913ef2ff9d670ba20815
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-13 17:54:54 +0300

    Frontend Files added

--- server.py
+++ server.py
@@ -1,11 +1,9 @@
 from flask import Flask,render_template,url_for
 app = Flask(__name__)
 
-print(__name__)
-
-@app.route('/<username>/<int:post_id>')
-def hello_world(username=None,post_id=None):
-    return render_template('index.html', name = username,post_id = post_id)
+@app.route('/')
+def myhome():
+    return render_template('index.html')
 
 @app.route('/about.html')
 def aboutme():
