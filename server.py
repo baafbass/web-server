8e7cf6245e39d82eaa19d74f2c33360ae2b64d94
ABDOUL FARIDE BASSIROU ALZOUMA <abdoulfaridbassirou7898@gmail.com>
2023-09-14 13:00:46 +0300

    Caching data from the form contact

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request,redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -26,3 +26,11 @@ def portfolio():
 def contact():
     return render_template('contact.html')
 
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        return redirect('formresult1.html')
+    else:
+        return 'Something went wrong. Try again'
